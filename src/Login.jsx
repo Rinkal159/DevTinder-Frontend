df596b7a80d3a46f52260006fcca56442c8bbc71
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-22 15:39:46 +0530

    Ended login page work with Error handling window.

--- src/Login.jsx
+++ src/Login.jsx
@@ -7,9 +7,10 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-
   const [email, setEmail] = useState("anushka@gmail.com");
   const [passWord, setPassword] = useState("Anushka@123");
+
+  const [err, setErr] = useState([]);
 
   const dispatch = useDispatch();
 
@@ -31,55 +32,75 @@ export default function Login() {
       alert("User logged in successfully!");
       dispatch(addUser(res.data.data));
 
-      return navigate("/index")
-
+      return navigate("/index");
     } catch (err) {
-      console.log(`Something went wrong! ${err}`);
+      const errors = err.response?.data?.message || err.message;
+
+      setErr((prevErr) => [...prevErr, errors]);
     }
   }
 
   return (
-    <div className="flex flex-col justify-center items-center flex-grow">
-      <div className="card  bg-base-100 shadow-sm w-full max-w-sm bg-black hover:shadow-2xl">
-        <div className="card-body">
-          <h2 className="text-3xl font-bold text-center mb-8 text-white">
-            Login
-          </h2>
-          <input
-            className="inputLogin"
-            type="text"
-            name="Email"
-            id="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            className="inputLogin"
-            type="text"
-            name="Password"
-            id="password"
-            placeholder="Password"
-            value={passWord}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+    
+      <div className="flex flex-col justify-center items-center flex-grow">
+        <div className="card  bg-base-100 shadow-sm w-full max-w-sm bg-black hover:shadow-2xl">
+          <div className="card-body">
+            <h2 className="text-3xl font-bold text-center mb-8 text-white">
+              Login
+            </h2>
+            <input
+              className="inputLogin"
+              type="text"
+              name="Email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="inputLogin"
+              type="text"
+              name="Password"
+              id="password"
+              placeholder="Password"
+              value={passWord}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-          <a
-            href="#"
-            className="hover:underline text-center text-[#0466c8] pt-2 pb-3"
-          >
-            Forgot password?
-          </a>
+            <a
+              href="#"
+              className="hover:underline text-center text-[#0466c8] pt-2 pb-3"
+            >
+              Forgot password?
+            </a>
 
-          <button
-            onClick={handleClick}
-            className="btn btn-primary btn-block bg-[#0466c8] hover:bg-[#0353a4]"
-          >
-            setAuthCookie()
-          </button>
+            <button
+              onClick={handleClick}
+              className="btn btn-primary btn-block bg-[#0466c8] hover:bg-[#0353a4]"
+            >
+              setAuthCookie()
+            </button>
+          </div>
         </div>
+
+         {err.length > 0 && (
+        <div className="outer-error">
+          <div className="inner-error">
+            <button className="close-btn" onClick={() => setErr([])}>
+              <i class="fa-solid fa-xmark"></i>
+            </button>
+            <h2 className="err-heading">Error</h2>
+            <ul>
+              {err.map((li, i) => (
+                <li className="errors" key={i}>{li}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )} 
       </div>
+
       
-    </div>
+    
   );
 }
