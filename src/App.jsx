6f53c176d3882176865b93d1148ec31b46cfe1aa
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-22 15:31:51 +0530

    App-The heart, Added demo profile and settings route.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Signup from "./Signup";
 import Login from "./Login";
 import Index from "./index.jsx";
 import Dashboard from "./dashboard.jsx";
+import Profile from "./Profile.jsx";
+import Settings from "./Settings.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
@@ -18,6 +20,8 @@ export default function App() {
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/settings" element={<Settings/>}/>
           </Route>
         </Routes>
       </Provider>
