b5229867feb73281039bb49d9ba93e6e5006242e
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-22 15:38:25 +0530

    If the user is logged in then init() won't display.

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,25 @@
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Index() {
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    function handleNavigate() {
-        return navigate("/signup")
-    }
-
+  function handleNavigate() {
+    return navigate("/signup");
+  }
 
   return (
     <div className="flex flex-col gap-y-8 justify-center items-center h-screen flex-grow">
       <h1 className="text-7xl font-black">Find Your REDADME of Life.</h1>
-      <button onClick={handleNavigate} className="btn btn-neutral btn-outline rounded-full text-2xl px-7 py-7">
-        Init()
-      </button>
+      {!user && (
+        <button
+          onClick={handleNavigate}
+          className="btn btn-neutral btn-outline rounded-full text-2xl px-7 py-7"
+        >
+          Init()
+        </button>
+      )}
     </div>
   );
 }
