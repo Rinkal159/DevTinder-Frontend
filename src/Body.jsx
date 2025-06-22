ee2c0db4c0227d3f988a4c36d2ababaa6347aca2
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-22 15:35:38 +0530

    Added functionality of not lossing data of user by refresh until the token expires or user logged out, if user is not present in redux store, only then after do the check.

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,15 +1,42 @@
 import Footer from "./Footer";
 import NavBar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "./features/user/userSlice";
+import { useEffect } from "react";
 
 export default function Body() {
+  const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  const userData = useSelector((state) => state.user);
+
+  async function getUser() {
+    try {
+      const user = await axios.get("http://localhost:3002/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(user.data));
+      
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  useEffect(() => {
+    if (!userData) {
+      getUser();
+    }
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col ">
       <NavBar />
       <main className="flex-grow flex flex-col">
-      <Outlet />
+        <Outlet />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
