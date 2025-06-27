c7396a8ec1cdbd76b1f228cf97da15a2749c0fc9
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-27 11:52:35 +0530

    Changed default email and password, navbar is now no more absolute so changed margin according to that.

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,10 +5,11 @@ import { useDispatch } from "react-redux";
 import { addUser } from "./features/user/userSlice";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  const [email, setEmail] = useState("anushka@gmail.com");
-  const [passWord, setPassword] = useState("Anushka@123");
+  const [email, setEmail] = useState("rinkal@gmail.com");
+  const [passWord, setPassword] = useState("Rinkal@123");
 
   const [err, setErr] = useState([]);
 
@@ -32,7 +33,9 @@ export default function Login() {
       alert("User logged in successfully!");
       dispatch(addUser(res.data.data));
 
-      return navigate("/index");
+      console.log(res.data.data);
+
+      return navigate("/feed");
     } catch (err) {
       const errors = err.response?.data?.message || err.message;
 
@@ -41,49 +44,48 @@ export default function Login() {
   }
 
   return (
-    
-      <div className="flex flex-col justify-center items-center flex-grow">
-        <div className="card  bg-base-100 shadow-sm w-full max-w-sm bg-black hover:shadow-2xl">
-          <div className="card-body">
-            <h2 className="text-3xl font-bold text-center mb-8 text-white">
-              Login
-            </h2>
-            <input
-              className="inputLogin"
-              type="text"
-              name="Email"
-              id="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              className="inputLogin"
-              type="text"
-              name="Password"
-              id="password"
-              placeholder="Password"
-              value={passWord}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+    <div className="flex flex-col h-[calc(100vh-70px)] justify-center items-center">
+      <div className="card bg-base-100 shadow-sm w-full max-w-sm bg-black hover:shadow-2xl">
+        <div className="card-body">
+          <h2 className="text-3xl font-bold text-center mb-8 text-white">
+            Login
+          </h2>
+          <input
+            className="inputLogin"
+            type="text"
+            name="Email"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="inputLogin"
+            type="text"
+            name="Password"
+            id="password"
+            placeholder="Password"
+            value={passWord}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-            <a
-              href="#"
-              className="hover:underline text-center text-[#0466c8] pt-2 pb-3"
-            >
-              Forgot password?
-            </a>
+          <a
+            href="#"
+            className="hover:underline text-center text-[#0466c8] pt-2 pb-3"
+          >
+            Forgot password?
+          </a>
 
-            <button
-              onClick={handleClick}
-              className="btn btn-primary btn-block bg-[#0466c8] hover:bg-[#0353a4]"
-            >
-              setAuthCookie()
-            </button>
-          </div>
+          <button
+            onClick={handleClick}
+            className="btn btn-primary btn-block bg-[#0466c8] hover:bg-[#0353a4]"
+          >
+            setAuthCookie()
+          </button>
         </div>
+      </div>
 
-         {err.length > 0 && (
+      {err.length > 0 && (
         <div className="outer-error">
           <div className="inner-error">
             <button className="close-btn" onClick={() => setErr([])}>
@@ -92,15 +94,14 @@ export default function Login() {
             <h2 className="err-heading">Error</h2>
             <ul>
               {err.map((li, i) => (
-                <li className="errors" key={i}>{li}</li>
+                <li className="errors" key={i}>
+                  {li}
+                </li>
               ))}
             </ul>
           </div>
         </div>
-      )} 
-      </div>
-
-      
-    
+      )}
+    </div>
   );
 }
