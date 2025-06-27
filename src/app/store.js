f867ee6712de50b132a7abb475fcbbcb7826b677
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-27 12:03:21 +0530

    Added feed state.

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import feedReducer from "../features/feed/feedSlice"
 
 export const store = configureStore({
     reducer: {
-        user: userReducer
+        user: userReducer,
+        feed : feedReducer
     }
 });
