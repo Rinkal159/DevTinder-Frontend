d56d7c33309ed55b63e4cc612b80ad3f5b98b439
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-27 11:55:43 +0530

    Removed absolute from navbar styles, improved profile icon by placing image when user is logged in, and when user logged out, the feed is cleared

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import "./index.css";
+import { addFeed } from "./features/feed/feedSlice";
 
 function Logout({ cancel, logout }) {
   return (
@@ -36,19 +38,25 @@ export default function NavBar() {
   const [logout, setLogout] = useState(false);
 
   async function handleLogout() {
-    await axios.post(
-      "http://localhost:3002/logout",
-      {},
-      {
-        withCredentials: true,
-      }
-    );
-    dispatch(addUser(null));
-    setLogout(false);
+    try {
+      await axios.post(
+        "http://localhost:3002/logout",
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(addUser(null));
+      dispatch(addFeed(null))
+      setLogout(false);
 
-    alert("User Logged out Successfully!");
+      alert("User Logged out Successfully!");
 
-    return navigate("/index");
+      return navigate("/index");
+    } catch (err) {
+      console.log(err.message);
+      
+    }
   }
 
   function goToLogin() {
@@ -56,65 +64,68 @@ export default function NavBar() {
   }
 
   return (
-    <div className="navbar bg-base-300 shadow-sm absolute top-0 z-10 bg-black text-white z-0">
-      <div className="flex-1">
-        <Link to={"/index"} className="btn btn-ghost text-2xl">
-          <i className="fa-solid fa-code"></i>DevTinder
-        </Link>
-      </div>
+    <div>
+      <div className="navbar">
+        <div className="flex-1">
+          <Link to={"/index"} className="btn btn-ghost text-2xl">
+            <i className="fa-solid fa-code"></i>DevTinder
+          </Link>
+        </div>
 
-      {state ? (
-        <div className="flex gap-2 items-center">
-          {state && <p>Welcome, {state.firstName}</p>}
+        {state ? (
+          <div className="flex gap-2 items-center">
+            {state && <p>Welcome, {state.firstName}</p>}
 
-          <div className="dropdown dropdown-end mx-4">
-            <div tabIndex={0} role="button" className="w-10">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
+            <div className="dropdown dropdown-end mx-4">
+              <div tabIndex={0} role="button" className="w-14">
+                <img
+                    src={state.img}
+                    alt={`${state.firstName} ${state.lastName}`}
+                    className="rounded-full"
+                  />
+              </div>
+
+              {!logout && (
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                >
+                  <li className="hover:bg-black">
+                    <Link to={"/profile"} className="justify-between">
+                      Profile
+                      <span className="badge">New</span>
+                    </Link>
+                  </li>
+                  <li className="hover:bg-black">
+                    <Link to={"/settings"}>Settings</Link>
+                  </li>
+                  <li className="hover:bg-black">
+                    <a onClick={() => setLogout(true)}>Logout</a>
+                  </li>
+                </ul>
+              )}
             </div>
-
-            {!logout && (
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-              >
-                <li className="hover:bg-black">
-                  <Link to={"/profile"} className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </Link>
-                </li>
-                <li className="hover:bg-black">
-                  <Link to={"/settings"} >Settings</Link>
-                </li>
-                <li className="hover:bg-black">
-                  <a onClick={() => setLogout(true)}>Logout</a>
-                </li>
-              </ul>
-            )}
           </div>
-        </div>
-      ) : (
-        <div>
-          <button
-            onClick={goToLogin}
-            className="btn btn-neutral rounded-full bg-[#0466c8] hover:bg-[#0054b1] px-5 mr-4"
-          >
-            LOGIN
-          </button>
-        </div>
-      )}
+        ) : (
+          <div>
+            <button
+              onClick={goToLogin}
+              className="btn btn-neutral rounded-full bg-[#0466c8] hover:bg-[#0054b1] px-5 mr-4"
+            >
+              LOGIN
+            </button>
+          </div>
+        )}
 
-      {logout && (
-        <Logout
-          cancel={() => {
-            setLogout(false);
-          }}
-          logout={handleLogout}
-        />
-      )}
+        {logout && (
+          <Logout
+            cancel={() => {
+              setLogout(false);
+            }}
+            logout={handleLogout}
+          />
+        )}
+      </div>
     </div>
   );
 }
