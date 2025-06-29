2959106ee74b02b34d0ca53e63bab8947e3506f6
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-29 09:01:52 +0530

    Add sendReq : users who received the requests, receivedRreq : users who sent the requests and connections : i or user accepted the requets ....reducers.

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
-import feedReducer from "../features/feed/feedSlice"
+import feedReducer from "../features/feed/feedSlice";
+import sendReqReducer from "../features/sendReq/sendRequestSlice"
+import receivedReqReducer from "../features/receivedReq/receivedReq"
+import connectionsReducer from "../features/connections/connectionsSlice"
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
-        feed : feedReducer
+        feed : feedReducer,
+        sendReq : sendReqReducer,
+        receivedReq : receivedReqReducer,
+        connections : connectionsReducer
     }
 });
