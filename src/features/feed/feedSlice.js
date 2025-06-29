fbb3adf29eaf09fa7ca3028298e95db908e1f5f5
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-29 09:03:56 +0530

    Added removeFeed reducer to remove the the user from the feed when i click on ignore or interested.

--- src/features/feed/feedSlice.js
+++ src/features/feed/feedSlice.js
@@ -2,13 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const feedSlice = createSlice({
     name:"feed",
-    initialState:null,
+    initialState:[],
     reducers:{
         addFeed : (state, action) => {
             return action.payload;
+        },
+        removeFeed : (state, action) => {
+            if(!state) return state;
+            return state.filter((state) => state._id !== action.payload)
         }
     }
 })
 
-export const {addFeed} = feedSlice.actions;
+export const {addFeed, removeFeed} = feedSlice.actions;
 export default feedSlice.reducer;
