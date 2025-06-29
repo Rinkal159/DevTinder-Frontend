442ad1bd8bb26d6ebc58103f7639ef883b60a7fe
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-29 17:08:20 +0530

    img and name container for pushed, pulled and merged requests.

---
+++ src/Identity.jsx
@@ -0,0 +1,17 @@
+export default function Identity({user, showUserProfile}) {
+  return (
+    <div onClick={() => showUserProfile(user)} className="flex items-center gap-2">
+      <img
+        className="h-12 w-12 rounded-full cursor-pointer"
+        src={user.img}
+        alt={`${user.firstName} ${user.lastName}`}
+      />
+      <p
+        
+        className="text-black cursor-pointer hover:font-semibold"
+      >
+        {user.firstName} {user.lastName}
+      </p>
+    </div>
+  );
+}
