982987e6a2fb37e17577df4a1f942e2d9c26b64c
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-29 09:19:14 +0530

    Reducers to manage sent requests. there are two states, one for bunch requests handling and one to display individual requests on showSentProfile page...

---
+++ src/features/sendReq/sendRequestSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    bunch: [],
+    individual: []
+}
+
+export const sendReqSlice = createSlice({
+    name: "sendReq",
+    initialState,
+    reducers: {
+
+        // add all requests to bunch from axios res
+        addSendReq: (state, action) => {
+            // push the new user (push action.payload), not override(return action.payload)
+
+            const { _id } = action.payload;
+
+            const existedUsers = state.bunch?.some((user) => user._id === _id); //'some' method return false if condition never satisfied, but when condition satisfies, it return true and stops iterating, true means there is duplicated users ditacted so in the if condition true becomes false thus the block will never execute and new user won't push in the array of state beacuse it is already there(duplicated)
+
+            if (!existedUsers) {
+                state.bunch.push(action.payload);
+            }
+        },
+
+        // clear bunch and make it 'empty' (not null) after user is logging out.
+        setSendReqStateEmpty: (state, action) => {
+            state.bunch = action.payload;
+        },
+
+        //add the profile of user in individual state when the user clicks on the name of the user
+        addSendReqProfile: (state, action) => {
+            state.individual = action.payload;
+        },
+    }
+})
+
+export const { addSendReq, setSendReqStateEmpty, addSendReqProfile } = sendReqSlice.actions;
+export default sendReqSlice.reducer;
+
