21c9bd3a3f8bc7db2c79c5338ab4c3f86064bbed
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:44:25 +0530

    Identity done with proper image appearing.

--- src/Identity.jsx
+++ src/Identity.jsx
@@ -1,8 +1,10 @@
+
+
 export default function Identity({user, showUserProfile}) {
   return (
     <div onClick={() => showUserProfile(user)} className="flex items-center gap-2">
       <img
-        className="h-12 w-12 rounded-full cursor-pointer"
+        className="h-12 w-12 rounded-full cursor-pointer object-cover"
         src={user.img}
         alt={`${user.firstName} ${user.lastName}`}
       />
