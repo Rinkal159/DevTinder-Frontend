317edd9f6729eac0bd87f122e7967de734f86e22
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 18:00:59 +0530

    Added redux persist and done its setup.

--- src/app/store.js
+++ src/app/store.js
@@ -4,13 +4,27 @@ import feedReducer from "../features/feed/feedSlice";
 import sendReqReducer from "../features/sendReq/sendRequestSlice"
 import receivedReqReducer from "../features/receivedReq/receivedReq"
 import connectionsReducer from "../features/connections/connectionsSlice"
+import storage from "redux-persist/lib/storage"
+import { persistStore, persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const reducers = combineReducers({
+    user: userReducer,
+    feed: feedReducer,
+    sendReq: sendReqReducer,
+    receivedReq: receivedReqReducer,
+    connections: connectionsReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        feed : feedReducer,
-        sendReq : sendReqReducer,
-        receivedReq : receivedReqReducer,
-        connections : connectionsReducer
-    }
+    reducer: persistedReducer
 });
+
+export const persister = persistStore(store);
