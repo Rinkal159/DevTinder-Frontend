7014b054fe04bdd5c09ce5d63e983367dfa5a7ce
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:41:05 +0530

    Removed getUser as redux persist rehydrates userdata after refreshing.

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,32 +1,9 @@
 import Footer from "./Footer";
 import NavBar from "./Navbar";
 import { Outlet } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState } from "react";
-
-import getMyUser from "./API_Calling/getMyUser";
 
 export default function Body() {
-  const [err, setErr] = useState([]);
-
-  const dispatch = useDispatch();
-  const userData = useSelector((state) => state.user);
-
-
-  // get the user
-  async function getUser() {
-    await getMyUser(dispatch, setErr);
-
-  }
-
-  useEffect(() => {
-    if (!userData) {
-      getUser();
-    }
-  }, []);
-
   return (
-    <div>
       <div className="min-h-screen flex flex-col">
         <NavBar />
         <div className="flex-grow">
@@ -34,26 +11,5 @@ export default function Body() {
         </div>
         <Footer />
       </div>
-
-
-      {/* error */}
-      {err.length > 0 && (
-        <div className="outer-error">
-          <div className="inner-error">
-            <button className="close-btn" onClick={() => setErr([])}>
-              <i class="fa-solid fa-xmark"></i>
-            </button>
-            <h2 className="err-heading">Error</h2>
-            <ul>
-              {err.map((li, i) => (
-                <li className="errors" key={i}>
-                  {li}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      )}
-    </div>
   );
 }
