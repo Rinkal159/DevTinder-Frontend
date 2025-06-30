93cbc58f7452c3d13b34be963f994941eb7f6b7d
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:58:45 +0530

    After each accept, add that user in connections list.

--- src/API_Calling/acceptOrRejectReq.js
+++ src/API_Calling/acceptOrRejectReq.js
@@ -1,17 +1,25 @@
 import axios from "axios";
 import { removeReq } from "../features/receivedReq/receivedReq";
+import { addConnections } from "../features/connections/connectionsSlice";
 
-export default async function acceptOrRejectReq(status, user, dispatch, navigate, setErr) {
+export default async function acceptOrRejectReq(status, user, dispatch, setErr, navigate) {
     try {
+
+      console.log("at axios : " + user._id);
+      
           const res = await axios.post(
-            `http://localhost:3002/request/review/${status}/${user}`,
+            `http://localhost:3002/request/review/${status}/${user._id}`,
             {},
             { withCredentials: true }
           );
 
           alert(`${res.data.data.firstName} : ${status}`)
     
-          dispatch(removeReq(user));
+          dispatch(removeReq(user._id));
+
+          if(status==="Accept") {
+            dispatch(addConnections(user))
+          }
     
           return navigate("/feed");
     
