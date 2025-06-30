df8b638ae9774ebb319a64ebd902025193b14bfc
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:42:24 +0530

    A separate Error file that is for all error windows.

---
+++ src/Error.jsx
@@ -0,0 +1,20 @@
+import "./index.css";
+export default function Error({ err, setErr }) {
+  return (
+    <div className="outer-error">
+      <div className="inner-error">
+        <button className="close-btn" onClick={() => setErr([])}>
+          <i className="fa-solid fa-xmark"></i>
+        </button>
+        <h2 className="err-heading">Error</h2>
+        <ul>
+          {err.map((li, i) => (
+            <li className="errors" key={i}>
+              {li}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
