3e0a2378f6359227a895a12572c8d31576cc7e2b
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:47:02 +0530

    Imported Error and removed former error components.

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 import login from "./API_Calling/login";
+import Error from "./Error";
 
 export default function Login() {
   const [email, setEmail] = useState("rinkal@gmail.com");
@@ -23,6 +24,9 @@ export default function Login() {
 
   return (
     <div className="flex flex-col h-[calc(100vh-70px)] justify-center items-center">
+      {/* Error */}
+      {err.length > 0 && <Error err={err} setErr={setErr} />}
+
       <div className="card bg-base-100 shadow-sm w-full max-w-sm bg-black hover:shadow-2xl">
         <div className="card-body">
           <h2 className="text-3xl font-bold text-center mb-8 text-white">
@@ -59,25 +63,6 @@ export default function Login() {
           </button>
         </div>
       </div>
-
-      {/* Error */}
-      {err.length > 0 && (
-        <div className="outer-error">
-          <div className="inner-error">
-            <button className="close-btn" onClick={() => setErr([])}>
-              <i class="fa-solid fa-xmark"></i>
-            </button>
-            <h2 className="err-heading">Error</h2>
-            <ul>
-              {err.map((li, i) => (
-                <li className="errors" key={i}>
-                  {li}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
