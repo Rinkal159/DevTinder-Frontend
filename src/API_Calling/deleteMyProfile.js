485509aec6e16ff55e784f78c303bbc3722785b4
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:59:44 +0530

    Delete call : Delete profile

---
+++ src/API_Calling/deleteMyProfile.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export default async function deleteMyProfile(setRemove, setErr) {
+    try {
+
+        const res = await axios.delete("http://localhost:3002/profile/delete", {
+            withCredentials: true
+        });
+
+        setRemove(false);
+
+    } catch (err) {
+        const error = err.response?.data?.message || err.message;
+        setErr((prevErr) => [...prevErr, error]);
+    }
+}
