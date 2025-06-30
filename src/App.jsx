e45801b2f068d10c7c650e54d600d8e44e80c1bd
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:40:07 +0530

    Added updateProfile and updatePassword routes.

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,39 @@ import Body from "./Body";
 import Signup from "./Signup";
 import Login from "./Login";
 import Index from "./index.jsx";
-import Dashboard from "./dashboard.jsx";
 import Profile from "./Profile.jsx";
+import UpdateProfile from "./UpdateProfile.jsx";
+import UpdatePassword from "./UpdatePassword.jsx";
 import Settings from "./Settings.jsx";
 import Feed from "./Feed.jsx";
 import ShowSentProfile from "./ShowSentProfile.jsx";
-import ShowReceivedProfile from "./ShowReceivedProfile.jsx"
+import ShowReceivedProfile from "./ShowReceivedProfile.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./app/store.js";
- 
+
+import { BrowserRouter } from "react-router-dom";
+
 export default function App() {
   return (
     <div>
-      <Provider store={store}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Body />}>
             <Route path="/index" element={<Index />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/profile" element={<Profile/>}/>
-            <Route path="/settings" element={<Settings/>}/>
-            <Route path="/feed" element={<Feed/>}/>
-            <Route path="/sentUserProfile" element={<ShowSentProfile/>}/>
-            <Route path="/receivedUserProfile" element={<ShowReceivedProfile/>}/>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/updateProfile" element={<UpdateProfile />} />
+            <Route path="/updatePassword" element={<UpdatePassword />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/sentUserProfile" element={<ShowSentProfile />} />
+            <Route
+              path="/receivedUserProfile"
+              element={<ShowReceivedProfile />}
+            />
           </Route>
         </Routes>
-      </Provider>
+      </BrowserRouter>
     </div>
   );
 }
