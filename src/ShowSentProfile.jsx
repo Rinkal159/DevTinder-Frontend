68d3072e1d2632084cc35c96d65f3ed1eb695ed2
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:55:26 +0530

    New feat. : delete request from individual cards.

--- src/ShowSentProfile.jsx
+++ src/ShowSentProfile.jsx
@@ -1,18 +1,34 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ShowFeed from "./ShowFeed";
+import Error from "./Error";
 
+import deletReq from "./API_Calling/deleteReq";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ShowSentProfile() {
+  const user = useSelector((state) => state.sendReq);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const user = useSelector((state) => state.sendReq);
+  const [err, setErr] = useState([]);
 
-    return (
-        <div>
-            <ShowFeed
-                feedUsers={user.individual}
-                
-                
-            />
-        </div>
-    )
+  async function deleteReq(status) {
+    console.log(user.individual[0]);
+
+    await deletReq(status, user.individual[0], dispatch, navigate, setErr);
+  }
+
+  return (
+    <div>
+      {/* Error */}
+      {err.length > 0 && <Error err={err} setErr={setErr} />}
+
+      <ShowFeed
+        feedUsers={user.individual}
+        sendIgnoreReq={deleteReq}
+        ignore={"Delete"}
+      />
+    </div>
+  );
 }
