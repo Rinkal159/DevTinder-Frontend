11386a24f461aab520242cb3b3c82d5c74d296cf
rinkal159 <rinkalsingapuri@gmail.com>
2025-06-30 17:47:29 +0530

    PersistGate and persistor for redux-persist.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store, persister } from "./app/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persister}>
+        <App />
+        </PersistGate>
+      </Provider>
     </React.StrictMode>
-  </BrowserRouter>
 );
