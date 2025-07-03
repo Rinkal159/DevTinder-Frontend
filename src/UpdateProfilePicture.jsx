8d60e7c5e94204b06e42dfd827edd99779d9f55e
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-03 18:38:47 +0530

    Update profile picture page that updates user's profile picture.

---
+++ src/UpdateProfilePicture.jsx
@@ -0,0 +1,91 @@
+import { useDispatch, useSelector } from "react-redux";
+import ProfilePicture from "./ProfilePicture";
+import { useState } from "react";
+import updateProfilePicture from "./API_Calling/updateProfilePicture";
+import { useAuth0 } from "@auth0/auth0-react";
+import Error from "./Error";
+import SuccessMessage from "./SuccessMessage";
+
+export default function UpdateProfilePicture() {
+  const user = useSelector((state) => state.user);
+  const { getAccessTokenSilently } = useAuth0();
+  const dispatch = useDispatch();
+
+  const [img, setImg] = useState("");
+  const [previewImg, setPreviewImg] = useState(user.img || "");
+
+  const [load, setLoad] = useState(false);
+  const [pass, setPass] = useState(false);
+  const [err, setErr] = useState([]);
+
+  function handleImage(e) {
+    const file = e.target.files[0];
+    setImg(file);
+    const validPreview = URL.createObjectURL(file);
+    setPreviewImg(validPreview);
+  }
+
+  async function uploadImg() {
+    const formData = new FormData();
+    formData.append("img", img);
+
+    if (previewImg === user.img) {
+      setErr((prevErr) => [...prevErr, "input is void()"]);
+      return;
+    }
+
+    const token = await getAccessTokenSilently();
+
+    await updateProfilePicture(
+      token,
+      formData,
+      setLoad,
+      setPass,
+      setImg,
+      setPreviewImg,
+      dispatch,
+      setErr
+    );
+  }
+
+  return (
+    <div>
+      {/* Loading */}
+      {load && (
+        <div className="outer-logout">
+          <h1 className="text-lg font-semibold">
+            Updating Your Profile Picture...
+          </h1>
+        </div>
+      )}
+
+      {/* Password changed message */}
+      {pass && (
+        <SuccessMessage
+          setSuccess={setPass}
+          message="Profile Picture updated successfully!!"
+        />
+      )}
+
+      {/* Error */}
+      {err.length > 0 && <Error err={err} setErr={setErr} />}
+
+      <div className="w-full flex flex-col items-center gap-y-8">
+        <h1 className="navbar-headings pt-8">
+          &lt; Update Profile Picture / &gt;
+        </h1>
+
+        <ProfilePicture
+          imgSrc={previewImg}
+          alt={`${user.firstName} ${user.lastName}`}
+          stylingForIMGPreview="w-60 h-60 object-cover rounded-full"
+          handleImage={handleImage}
+        />
+
+        <button className="btn btn-outline" onClick={uploadImg}>
+          Update
+        </button>
+      </div>
+    </div>
+  );
+}
