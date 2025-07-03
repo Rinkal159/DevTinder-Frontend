d39d37527b1a56cd57783fd8520fc7e60c5e5de2
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-03 18:30:06 +0530

    Added token to axios call and now sends id, not user.

--- src/ShowSentProfile.jsx
+++ src/ShowSentProfile.jsx
@@ -5,8 +5,11 @@ import Error from "./Error";
 import deletReq from "./API_Calling/deleteReq";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function ShowSentProfile() {
+  const {getAccessTokenSilently} = useAuth0();
+
   const user = useSelector((state) => state.sendReq);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -14,21 +17,23 @@ export default function ShowSentProfile() {
   const [err, setErr] = useState([]);
 
   async function deleteReq(status) {
-    console.log(user.individual[0]);
+    const token = await getAccessTokenSilently();
 
-    await deletReq(status, user.individual[0], dispatch, navigate, setErr);
+    await deletReq(token,status, user.individual[0]._id, dispatch, navigate, setErr);
   }
 
   return (
     <div>
-      {/* Error */}
-      {err.length > 0 && <Error err={err} setErr={setErr} />}
+      
 
       <ShowFeed
         feedUsers={user.individual}
         sendIgnoreReq={deleteReq}
         ignore={"Delete"}
       />
+
+      {/* Error */}
+      {err.length > 0 && <Error err={err} setErr={setErr} />}
     </div>
   );
 }
