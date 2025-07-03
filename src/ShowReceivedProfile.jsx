47c1a0b02abb5a135b4ea5a92c8c200237d9f308
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-03 18:28:35 +0530

    Added token to axios call and now sends id, not user.

--- src/ShowReceivedProfile.jsx
+++ src/ShowReceivedProfile.jsx
@@ -5,8 +5,10 @@ import { useEffect, useState } from "react";
 import Error from "./Error";
 
 import acceptOrRejectReq from "./API_Calling/acceptOrRejectReq";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function ShowReceivedProfile() {
+  const {getAccessTokenSilently} = useAuth0();
   const user = useSelector((state) => state.receivedReq);
 
   const [err, setErr] = useState([]);
@@ -16,9 +18,12 @@ export default function ShowReceivedProfile() {
 
   // to accept or reject the individual request
   async function reviewReq(status) {
+    const token = await getAccessTokenSilently();
+
     await acceptOrRejectReq(
+      token,
       status,
-      user.individual[0],
+      user.individual[0]._id,
       dispatch,
       setErr,
       navigate
@@ -27,8 +32,7 @@ export default function ShowReceivedProfile() {
 
   return (
     <div>
-      {/* Error */}
-      {err.length > 0 && <Error err={err} setErr={setErr} />}
+      
 
       <ShowFeed
         feedUsers={user.individual}
@@ -37,6 +41,9 @@ export default function ShowReceivedProfile() {
         ignore={"Reject"}
         interested={"Accept"}
       />
+
+      {/* Error */}
+      {err.length > 0 && <Error err={err} setErr={setErr} />}
     </div>
   );
 }
