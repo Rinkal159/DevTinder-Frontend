7050266c911144ae8228eca9bcdb4f82c8e0ddad
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-07 17:53:11 +0530

    Now tailwind css layer componenets instead of inline styling.

--- src/ShowFeed.jsx
+++ src/ShowFeed.jsx
@@ -1,23 +1,29 @@
-export default function ShowFeed({ feedUsers, sendIgnoreReq, sendInterestedReq, load, ignore, interested }) {
-  if (load)
-    return (
-      <div className="pt-8 text-center text-2xl">Fetching Your Feed...</div>
-    );
+import ToggleButton from "./ToggleButton";
+
+export default function ShowFeed({
+  feedUsers,
+  sendIgnoreReq,
+  sendInterestedReq,
+  load,
+  ignore,
+  interested,
+}) {
+  if (load) return <div className="load">Fetching Your Feed...</div>;
 
   return (
-    <div className=" flex  flex-col items-center mt-8 mb-8">
+    <div className="user-card-bg">
       {feedUsers && feedUsers.length > 0 ? (
         <div
-          className="flex flex-col w-[20rem] bg-black items-center "
+          className="user-card "
           key={feedUsers[0]._id}
         >
           <img
-            className="h-[20rem] w-[20rem] object-cover"
+            className="user-card-img"
             src={feedUsers[0].img}
             alt={`${feedUsers[0].firstName} ${feedUsers[0].lastName}`}
           />
 
-          <div className="flex flex-col gap-y-4 bg-black text-md pt-4">
+          <div className="main-user-info">
             <div className="flex gap-x-8 justify-between">
               <h1 className="text-lg h1">{`${feedUsers[0].firstName.toUpperCase()} ${feedUsers[0].lastName.toUpperCase()}`}</h1>
               <h1 className="h1">
@@ -27,24 +33,24 @@ export default function ShowFeed({ feedUsers, sendIgnoreReq, sendInterestedReq, 
             </div>
 
             <h1 className="h1">
-              <i className="fa-solid fa-location-dot"></i>&nbsp;State :{" "}
+              <i className="fa-solid fa-location-dot"></i>&nbsp;&nbsp;State :{" "}
               {feedUsers[0].state}
             </h1>
 
             <h1 className="h1">
-              <i className="fa-solid fa-person"></i>&nbsp;Gender :{" "}
+              <i className="fa-solid fa-person"></i>&nbsp;&nbsp;Gender :{" "}
               {feedUsers[0].gender}
             </h1>
             <h1 className="h1">
-              <i className="fa-solid fa-briefcase"></i>&nbsp;Occupation :{" "}
+              <i className="fa-solid fa-briefcase"></i>&nbsp;&nbsp;Occupation :{" "}
               {feedUsers[0].occupation}
             </h1>
             <h1 className="h1">
-              <i className="fa-solid fa-laptop-code"></i>&nbsp;Tech Stacks :{" "}
+              <i className="fa-solid fa-laptop-code"></i>&nbsp;&nbsp;Tech Stacks :{" "}
               {feedUsers[0].techStacks}
             </h1>
             <h1 className="h1">
-              <i className="fa-solid fa-bullseye"></i>&nbsp;My Goals :{" "}
+              <i className="fa-solid fa-bullseye"></i>&nbsp;&nbsp;My Goals :{" "}
               {feedUsers[0].goals}
             </h1>
           </div>
@@ -53,7 +59,7 @@ export default function ShowFeed({ feedUsers, sendIgnoreReq, sendInterestedReq, 
             {/* ignore */}
             <button
               onClick={() => sendIgnoreReq(ignore)}
-              className="btn btn-outline ignore-btn border-red-500  text-red-500"
+              className=" ignore-btn "
             >
               <i className="fa-solid fa-xmark text-2xl inline-block"></i>
               &nbsp;{ignore}
@@ -62,18 +68,14 @@ export default function ShowFeed({ feedUsers, sendIgnoreReq, sendInterestedReq, 
             {/* interested */}
             <button
               onClick={() => sendInterestedReq(interested)}
-              className="btn btn-outline interested-btn text-green-300 border-green-500"
+              className=" interested-btn "
             >
               <i className="fa-solid fa-check text-2xl"></i>&nbsp;{interested}
             </button>
           </div>
         </div>
       ) : (
-        <div className=" text-center bg-black z-50 bg-opacity-50">
-          <div className="">
-            <h2 className="text-xl">No More Users To Show...</h2>
-          </div>
-        </div>
+            <h2 className="text-xl text-center text-defaultDark">No More Users To Show...</h2>
       )}
     </div>
   );
