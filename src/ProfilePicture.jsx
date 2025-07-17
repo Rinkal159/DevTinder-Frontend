04ac26d860a7cd687b9281d4ad7b9bacf21593cb
rinkal159 <rinkalsingapuri@gmail.com>
2025-07-17 18:22:09 +0530

    Some new styling.

--- src/ProfilePicture.jsx
+++ src/ProfilePicture.jsx
@@ -1,6 +1,6 @@
 export default function ProfilePicture({ imgSrc, alt, stylingForIMGPreview, handleImage }) {
   return (
-    <div className="flex flex-col gap-y-8 justify-center items-center">
+    <div className="flex flex-col gap-y-8 justify-center items-center shadow-profilePictureShadow p-8 rounded-xl">
       <img src={imgSrc} alt={alt} className={stylingForIMGPreview} />
       <input
         type="file"
